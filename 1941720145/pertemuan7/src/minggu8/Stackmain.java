5472aab8ff9d1db22bf1a008ec2cecec98ae1c5b
AhmadMaududiAzmi <maududi.azmi2702@gmail.com>
2020-03-26 11:41:35 +0700

    pertemuan 7+8 baru

---
+++ 1941720145/pertemuan7/src/minggu8/Stackmain.java
@@ -0,0 +1,17 @@
+package minggu8;
+public class Stackmain {
+    public static void main (String args[]) {
+        Stack tumpukan = new Stack(4);
+        
+        tumpukan.push(15);
+        tumpukan.push(31);
+        tumpukan.push(9);
+        tumpukan.push(12);
+        
+        tumpukan.print();
+        
+        tumpukan.pop();
+        tumpukan.peek();
+        tumpukan.print();
+    }
+}
